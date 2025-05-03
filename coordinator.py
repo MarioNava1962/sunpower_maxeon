db9bab6799b8d74e9a9df554d7c9b015c2045a38
geims12 <chiappe.giacomo@gmail.com>
2025-05-03 21:48:11 +0200

    defined logger

--- coordinator.py
+++ coordinator.py
@@ -1,9 +1,20 @@
+"""Coordinator for SunPower Maxeon integration."""
+
+import logging
 from datetime import timedelta
+
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator, UpdateFailed
+
 from .api import AsyncConfigEntryAuth
 
+_LOGGER = logging.getLogger(__name__)
+
+
 class SunPowerCoordinator(DataUpdateCoordinator):
+    """Coordinator to manage fetching data from the SunPower Maxeon API."""
+
     def __init__(self, hass, api: AsyncConfigEntryAuth):
+        """Initialize the coordinator."""
         self.api = api
         super().__init__(
             hass,
@@ -13,6 +24,7 @@ class SunPowerCoordinator(DataUpdateCoordinator):
         )
 
     async def _async_update_data(self):
+        """Fetch data from API endpoint."""
         try:
             return await self.api.async_get_systems()
         except Exception as err:
