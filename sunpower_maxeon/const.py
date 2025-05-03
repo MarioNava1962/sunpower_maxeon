fa3c1b3e0327bfeb964171f4ff4a3864a54b12df
geims12 <chiappe.giacomo@gmail.com>
2025-05-03 19:41:30 +0200

    Delete sunpower_maxeon directory

--- sunpower_maxeon/const.py
+++
@@ -1,7 +0,0 @@
-"""Constants for the SunPower Maxeon integration."""
-
-DOMAIN = "sunpower_maxeon"
-
-# TODO Update with your own urls
-OAUTH2_AUTHORIZE = "https://api.sunpower.maxeon.com/v1/authorize"
-OAUTH2_TOKEN = "https://api.sunpower.maxeon.com/v1/token"
