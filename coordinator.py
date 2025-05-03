7ed7434e5372ed46492989c9ff933c061497860f
geims12 <chiappe.giacomo@gmail.com>
2025-05-03 23:33:58 +0200

    Update coordinator.py

--- coordinator.py
+++ coordinator.py
@@ -43,7 +43,7 @@ class SunPowerCoordinator(DataUpdateCoordinator):
 
             # Merge both dictionaries
             #merged = {**system, **details_data}
-            return system
+            return system_data
 
         except Exception as err:
             _LOGGER.error("Failed to fetch system data: %s", err)
